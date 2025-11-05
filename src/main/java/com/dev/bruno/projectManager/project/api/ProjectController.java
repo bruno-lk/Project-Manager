d0a7ddf1e5abdbb473cbc8915a0905821381103a
Bruno Carvalho <brunoncarvalho@outlook.com>
2025-11-05 19:22:45 -0300

    criar controller para modulo de projeto

---
+++ src/main/java/com/dev/bruno/projectManager/project/api/ProjectController.java
@@ -0,0 +1,23 @@
+package com.dev.bruno.projectManager.project.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+    private final ICreateProjectUseCase createProjectUseCase;
+
+    public ProjectController(ICreateProjectUseCase createProjectUseCase) {
+        this.createProjectUseCase = createProjectUseCase;
+    }
+
+    @PostMapping
+    public UUID CreateProject(@RequestBody ProjectRecord record) throws Exception {
+        return createProjectUseCase.execute(record.name());
+    }
+}
